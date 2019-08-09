1fba444a566563715f213793a0a9f4e246e49ab5
vishu-garg <46370385+vishu-garg@users.noreply.github.com>
2019-08-10 01:43:58 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var mongoose=require('mongoose');
+var http=require('http');
+var routes=require('./route.js');
+
+var mongoDB='mongodb://localhost:27017/mppsc-ranklist-generator';
+mongoose.connect(mongoDB,{useNewUrlParser:true});
+var db=mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+var app=express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+
+var server = http.createServer((function(req,res){
+	app.use('/',routes);
+}));
+server.listen(3005);
